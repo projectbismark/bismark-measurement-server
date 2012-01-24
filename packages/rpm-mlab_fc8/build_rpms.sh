82084982ec01a1804ddf384071122046a6f97a02
Stephen Woodrow <srwoodrow@gmail.com>
2012-01-24 15:34:28 -0500

    Fix bugs and add additional error checks.

--- packages/rpm-mlab_fc8/build_rpms.sh
+++ packages/rpm-mlab_fc8/build_rpms.sh
@@ -49,37 +49,42 @@ function check_cmd()
     fi
 }
 
+relroot=$(dirname "$0")
+absroot="$PWD/$relroot"
+
 check_cmd 'rpm' 'rpm'
 check_cmd 'rpmdev-setuptree' 'rpmdevtools'
 
-rpmbuild_topdir=$(rpm --eval '%topdir')
+rpmbuild_topdir=$(rpm --eval '%{_topdir}')
 
 if [[ "$USER" != 'makerpm' ]] &&
         [[ "$(dirname $rpmbuild_topdir)" != "$HOME" ]]; then
     prompt_yn "User '$USER' doesn't look like a build account.
                Are you sure you want to set up an rpmbuild directory in this
-               user's home directory?" "N" || exit 1
+               user's home directory?" "N" || exit 2
 fi
 if [[ "$(dirname $rpmbuild_topdir)" == "$HOME" ]]; then
-    prompt_yn "This will destroy the contents of '$topdir'.
-               Do you want to continue?" "N" || exit 0
+    prompt_yn "This will destroy the contents of '$rpmbuild_topdir'.
+               Do you want to continue?" "N" || exit 2
 fi
 
 rpmdev-setuptree
 rpmdev-wipetree
 
-rpmbuild_topdir=$(rpm --eval '%topdir')
-rpmbuild_sources=$(rpm --eval '%sourcedir')
-rpmbuild_specs=$(rpm --eval '%specdir')
+rpmbuild_topdir=$(rpm --eval '%{_topdir}')
+rpmbuild_sources=$(rpm --eval '%{_sourcedir}')
+rpmbuild_specs=$(rpm --eval '%{_specdir}')
 
-for specdir in $(find ./ -mindepth 1 -maxdepth 1 -type d)
+for specdir in $(find $absroot -mindepth 1 -maxdepth 1 -type d)
 do
     pkg=$(basename $specdir)
-    ln -s "$specdir/$pkg.spec" "$rpmbuild_specs"
-    for srcfile in $(find $specdir -mindepth 1 -maxdepth 1 ! -name $pkg.spec)
-    do
-        cp -a "$srcfile" "$rpmbuild_sources"
-    done
+    if [[ -e "$specdir/$pkg.spec" ]]; then
+        ln -s "$specdir/$pkg.spec" "$rpmbuild_specs"
+        for srcfile in $(find $specdir -mindepth 1 -maxdepth 1 ! -name $pkg.spec)
+        do
+            cp -a "$srcfile" "$rpmbuild_sources"
+        done
+    fi
 done
 
 specfiles=$(find $rpmbuild_specs -mindepth 1 -maxdepth 1 -name *.spec)
