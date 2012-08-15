a142c9ee8e39ecc676fc32b26fa114e2640f364a
Stephen Woodrow <srwoodrow@gmail.com>
2012-08-14 21:21:05 -0400

    Update mserver_mgmt.py w/ changes from beachmont.

--- mserver-mgmt/mserver_mgmt.py
+++ mserver-mgmt/mserver_mgmt.py
@@ -26,10 +26,94 @@ SETTINGS_FILE = 'settings.json'
 with open(SETTINGS_FILE, 'r') as f:
     settings = json.load(f)
 
+def fqdn_matches_remote_addr(fqdn):
+    try:
+        if (bottle.request.remote_addr in settings['admin_ips'] or
+            bottle.request.remote_addr == socket.gethostbyname(fqdn + '.')):
+            return True
+    except IOError as ioe:
+        print(ioe)
+    return False
+
+def fqdn_authorized(db, fqdn):
+    try:
+        c = db.execute(
+                'SELECT * from authorized_fqdns where fqdn = ?;', (fqdn,))
+        if c.fetchone():
+            return True
+    except sqlite3.Error:
+        pass
+    return False
+
+def check_sqlite(db):
+    if not db.execute(
+            "SELECT name "
+            "FROM sqlite_master "
+            "WHERE type = 'table' and name = 'mservers'").fetchone():
+        db.execute(
+            "CREATE TABLE mservers "
+            "   (fqdn text primary key, "
+            "   ip text, "
+            "   last_seen integer, "
+            "   version text, "
+            "   puppet_version text);")
+        db.commit()
+    if not db.execute(
+            "SELECT name "
+            "FROM sqlite_master "
+            "WHERE type = 'table' and name = 'authorized_fqdns'").fetchone():
+        db.execute("CREATE TABLE authorized_fqdns (fqdn text primary key);")
+        db.commit()
+    if not db.execute(
+            "SELECT name "
+            "FROM sqlite_master "
+            "WHERE type = 'table' and name = 'mserver_performance'").fetchone():
+        db.execute(
+            "CREATE TABLE mserver_performance "
+            "   (key text primary key, "
+            "   fqdn_from text, "
+            "   fqdn_to text, "
+            "   utcdate text, "
+            "   throughput integer, "
+            "   jitter real, "
+            "   loss real, "
+            "   latency real);")
+        db.commit()
+
+def add_authorized_fqdns(db, additional_fqdns=[]):
+    plc = xmlrpclib.ServerProxy(
+            'https://www.planet-lab.org/PLCAPI/',
+            allow_none=True)
+
+    auth = settings['planetlab']['auth']
+    node_ids = plc.GetSlices(auth, {'name': 'gt_bismark'}, ['node_ids'])
+    node_list = plc.GetNodes(auth, {'node_id': node_ids[0]['node_ids']})
+    node_fqdns = [('bismark.gt.' + n['hostname']) for n in node_list]
+    node_fqdns.extend(additional_fqdns)
+
+    db.execute('DELETE FROM authorized_fqdns;');
+    db.executemany(
+            'INSERT INTO authorized_fqdns(fqdn) VALUES (?)',
+            ((n,) for n in node_fqdns))
+    db.commit()
+
+def resolve_admin_fqdns():
+    settings['admin_ips'] = []
+    for fqdn in settings['admin_fqdns']:
+        try:
+            settings['admin_ips'].append(socket.gethostbyname(fqdn))
+        except IOError as ioe:
+            print(ioe)
+
 class MserverMgmtApp(object):
     def __init__(self, bottle_app):
         self.post_response=[]
         self.bottle_app = bottle_app
+        db = sqlite3.connect(settings['dbfile'])
+        check_sqlite(db)
+        #add_authorized_fqdns(db, additional_fqdns=settings['admin_fqdns'])
+        resolve_admin_fqdns()
+        db.close()
 
     def post_hook(self, f):
         self.post_response.append(f)
@@ -100,10 +184,10 @@ def get_all_mservers(db):
     try:
         if 'text/html' in bottle.request.headers['accept']:
             return bottle.template('all_mservers', mservers=rows)
-        elif 'application/json' in bottle.request.header['accept']:
+        elif 'application/json' in bottle.request.headers['accept']:
             out = []
             for row in rows:
-                out.append('/mservers/' + row['fqdn'])
+                out.append({'url': '/mservers/' + row['fqdn'], 'fqdn': row['fqdn']})
             return({'mservers': out})
     except KeyError:
         pass
@@ -167,6 +251,80 @@ def post_mserver(db, mserver_fqdn):
         raise bottle.HTTPError(
                 400, "request must be formatted as application/json")
 
+@app.get('/mserver_performance/<key>')
+def get_mserver_perf(db, key):
+    c = db.execute('SELECT * from mserver_performance where key = ?;', (key,))
+    row = c.fetchone()
+    if row:
+        d = {}
+        for k in row.keys():
+            if k == 'key':
+                pass
+            elif not row['success'] and k in ['throughput', 'jitter', 'loss', 'latency']:
+                pass
+            else:
+                d[k] = row[k]
+        d['success'] = bool(d['success'])
+        return d
+    raise bottle.HTTPError(404)
+
+@app.post('/mserver_performance')
+@app.post('/mserver_performance/')
+def post_mserver_perf(db):
+    measurements = []
+    if bottle.request.json:
+        json_valid = True
+        if not isinstance(bottle.request.json, list):
+            raise bottle.HTTPError(400, "malformed request; must be a list of objects")
+        for measurement in bottle.request.json:
+            vals = {}
+            try:
+                for k in ['fqdn_from', 'fqdn_to', 'utcdate', 'success']:
+                    vals[k] = measurement[k]
+                if vals['success']:
+                    for k in ['throughput', 'jitter', 'loss', 'latency']:
+                        vals[k] = measurement[k]
+                if not fqdn_matches_remote_addr(vals['fqdn_from']):
+                    raise bottle.HTTPError(400, "FQDN mismatch")
+                if not datetime.datetime.strptime(vals['utcdate'], '%Y-%m-%d %H:%M:%S'):
+                    json_valid = False
+                if not isinstance(vals['success'], bool):
+                    json_valid = False
+            except (LookupError, ValueError, AttributeError):
+                json_valid = False
+            if json_valid:
+                measurements.append(vals)
+            else:
+                raise bottle.HTTPError(400, "malformed request")
+    else:
+        raise bottle.HTTPError(
+                415, "request must be formatted as application/json")
+
+    int_or_none = lambda x: int(x) if x is not None else x
+    float_or_none = lambda x: float(x) if x is not None else x
+    result_urls = []
+    try:
+        for m in measurements:
+            key = hashlib.sha1(''.join([m['fqdn_from'], m['fqdn_to'], m['utcdate']])).hexdigest()
+            result_urls.append('/mserver_performance/%s' % key)
+            db.execute(
+                    'INSERT INTO mserver_performance '
+                    '(key, fqdn_from, fqdn_to, utcdate, success, throughput, jitter, loss, latency) VALUES (?,?,?,?,?,?,?,?,?);',
+                    (key,
+                     m['fqdn_from'],
+                     m['fqdn_to'],
+                     m['utcdate'],
+                     m['success'],
+                     int_or_none(m.get('throughput', None)),
+                     float_or_none(m.get('jitter', None)),
+                     float_or_none(m.get('loss', None)),
+                     float_or_none(m.get('latency', None))))
+    except sqlite3.DatabaseError as e:
+        db.rollback()
+        raise bottle.HTTPError(500, "database error: " + str(e))
+    else:
+        db.commit()
+        return {'result_urls': result_urls}
 
 @app.get('/mservers/<mserver_fqdn>/hosts.allow')
 def get_mserver_hostsallow_json(db, mserver_fqdn):
@@ -179,76 +337,7 @@ def get_mserver_hostsallow_json(db, mserver_fqdn):
 ##########################
 
 
-def fqdn_matches_remote_addr(fqdn):
-    try:
-        if (bottle.request.remote_addr in settings['admin_ips'] or
-            bottle.request.remote_addr == socket.gethostbyname(fqdn + '.')):
-            return True
-    except IOError as ioe:
-        print(ioe)
-    return False
-
-def fqdn_authorized(db, fqdn):
-    try:
-        c = db.execute(
-                'SELECT * from authorized_fqdns where fqdn = ?;', (fqdn,))
-        if c.fetchone():
-            return True
-    except sqlite3.Error:
-        pass
-    return False
-
-def check_sqlite(db):
-    if not db.execute(
-            "SELECT name "
-            "FROM sqlite_master "
-            "WHERE type = 'table' and name = 'mservers'").fetchone():
-        db.execute(
-            "CREATE TABLE mservers "
-            "   (fqdn text primary key, "
-            "   ip text, "
-            "   last_seen integer, "
-            "   version text, "
-            "   puppet_version text);")
-        db.commit()
-    if not db.execute(
-            "SELECT name "
-            "FROM sqlite_master "
-            "WHERE type = 'table' and name = 'authorized_fqdns'").fetchone():
-        db.execute("CREATE TABLE authorized_fqdns (fqdn text primary key);")
-        db.commit()
-
-def add_authorized_fqdns(db, additional_fqdns=[]):
-    plc = xmlrpclib.ServerProxy(
-            'https://www.planet-lab.org/PLCAPI/',
-            allow_none=True)
-
-    auth = settings['planetlab']['auth']
-    node_ids = plc.GetSlices(auth, {'name': 'gt_bismark'}, ['node_ids'])
-    node_list = plc.GetNodes(auth, {'node_id': node_ids[0]['node_ids']})
-    node_fqdns = [('bismark.gt.' + n['hostname']) for n in node_list]
-    node_fqdns.extend(additional_fqdns)
-
-    db.execute('DELETE FROM authorized_fqdns;');
-    db.executemany(
-            'INSERT INTO authorized_fqdns(fqdn) VALUES (?)',
-            ((n,) for n in node_fqdns))
-    db.commit()
-
-def resolve_admin_fqdns():
-    settings['admin_ips'] = []
-    for fqdn in settings['admin_fqdns']:
-        try:
-            settings['admin_ips'].append(socket.gethostbyname(fqdn))
-        except IOError as ioe:
-            print(ioe)
-
 def main(debug=False):
-    db = sqlite3.connect(settings['dbfile'])
-    check_sqlite(db)
-    add_authorized_fqdns(db, additional_fqdns=settings['admin_fqdns'])
-    resolve_admin_fqdns()
-    db.close()
     if debug:
         bottle.debug()
         bottle.run(
