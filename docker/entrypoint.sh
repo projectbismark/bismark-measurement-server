92eeb2100255774ed208034f7df40962430e0f35
Nathan Kinkade <kinkade@measurementlab.net>
2019-06-03 17:02:18 -0600

    Implements a more deterministic and reliable approach to restarting the server if a port is not listening.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,9 +2,13 @@
 
 while true;
 do
-  if [ $(netstat -lntp | tail -n+3 | wc -l) != 8 ]; then
-	  /etc/init.d/bismark-mserver restart
-	  echo "Bismark MServer restarted."
-  fi
+  for port in 1100 1101 1102 1430 5001 9000 12865 55005; do
+    ss -ltn | awk '{print $4}' | grep $port
+    if [[ "$?" -ne "0" ]]; then
+      /etc/init.d/bismark-mserver restart
+      echo "Bismark MServer restarted."
+      break
+    fi
+  done
   sleep 5
 done
