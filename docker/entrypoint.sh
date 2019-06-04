d85a83b7716fa983735b8dcdb2e8d76c319a3d20
Nathan Kinkade <kinkade@measurementlab.net>
2019-06-04 10:17:06 -0600

    Pipes output to /dev/null to avoid filling up the container logs with useless ss output.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 while true;
 do
   for port in 1100 1101 1102 1430 5001 9000 12865 55005; do
-    ss -ltn | awk '{print $4}' | grep $port
+    ss -ltn | awk '{print $4}' | grep $port > /dev/null
     if [[ "$?" -ne "0" ]]; then
       /etc/init.d/bismark-mserver restart
       echo "Bismark MServer restarted."
